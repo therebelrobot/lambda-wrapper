5e0103f0782c9358d2b334feb742c8b79291508a
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-05-09 16:55:23 -0700

    added log

--- wrapper.js
+++ wrapper.js
@@ -1,4 +1,5 @@
 const getProp = require('@f/get-prop');
+const noop = () => null;
 
 const respond = (callback) => (statusCode, body, opts) => {
   opts = opts || {};
@@ -8,8 +9,10 @@ const respond = (callback) => (statusCode, body, opts) => {
 
 module.exports = (fn, opts) => async (event, context, callback) => {
   try {
+    let log;
+    if (verbose) { log = console.log; } else { log = noop; }
     const verbose = !!(getProp('queryStringParameters.verbose', event));
-    return await fn({ verbose, event, context, callback, respond: respond(callback) })
+    return await fn({ verbose, log, event, context, callback, respond: respond(callback) })
   } catch (e) {
     response = (opts && opts.surfaceErrors) ? { error: e.toString() } : null;
     return respond(callback)(500, response);
