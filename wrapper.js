b9f6051bd4da758d59f2a69a199bb0f1ae0d4fa4
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-05-09 17:01:41 -0700

    added multiform and json parsing

--- wrapper.js
+++ wrapper.js
@@ -1,4 +1,5 @@
 const getProp = require('@f/get-prop');
+const parse = require('lambda-multipart-promise');
 const noop = () => null;
 
 const respond = (callback) => (statusCode, body, opts) => {
@@ -9,10 +10,35 @@ const respond = (callback) => (statusCode, body, opts) => {
 
 module.exports = (fn, opts) => async (event, context, callback) => {
   try {
+    // Set Verbose flag
+    const verbose = !!(getProp('queryStringParameters.verbose', event));
+
+    // set up verbose console log
     let log;
     if (verbose) { log = console.log; } else { log = noop; }
-    const verbose = !!(getProp('queryStringParameters.verbose', event));
-    return await fn({ verbose, log, event, context, callback, respond: respond(callback) })
+
+    // parse multipart form data, if present
+    let multipartData;
+    if (event.headers['Content-Type'] && event.headers['Content-Type'].includes('multipart/form-data')) {
+      multipartData = await parse(event.body, event.headers, { verbose });
+    }
+
+    // parse json data, if present
+    let jsonData;
+    if (event.headers['Content-Type'] && event.headers['Content-Type'].includes('application/json')) {
+      jsonData = JSON.parse(event.body);
+    }
+
+    return await fn({
+      verbose,
+      log,
+      multipartData,
+      jsonData,
+      event,
+      context,
+      callback,
+      respond: respond(callback),
+    })
   } catch (e) {
     response = (opts && opts.surfaceErrors) ? { error: e.toString() } : null;
     return respond(callback)(500, response);
